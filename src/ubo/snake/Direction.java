4b89cd0fc09a65a185e720fa9952d7af9ad910b1
Mariam Ousmane Sall <e21308994@univ-brest.fr>
2014-02-03 17:34:37 +0100

    ajouter Direction.java

--- src/ubo/snake/Direction.java
+++ src/ubo/snake/Direction.java
@@ -2,9 +2,9 @@ package ubo.snake;
 
 public enum Direction {
 
-	VERS_LE_HAUT,
-    VERS_LA_DROITE,
-    VERS_LE_BAS,
-    VERS_LA_GAUCHE;
+	VERS_LE_HAUT,//direction du serpent vers le haut
+    VERS_LA_DROITE,//direction du serpent vers la droite
+    VERS_LE_BAS,//direction du serpent vers le bas
+    VERS_LA_GAUCHE;//direction du serpent vers la gauche
 	
 }
